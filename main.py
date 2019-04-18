959725e6fe88012cc4ad94359ec294d3c8f853d7
souvikbagchi <souvik.bagchi@gmail.com>
2019-04-18 09:10:04 -0500

    test

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import sys
 # example : python main.py /Users/mayank/documents/ppmi_swedd_dl_mayank/PPMI
 
 rootdir = sys.argv[1]
+# rootdir = "/Users/souvik/Documents/NU/Lectures/Spring2019/EECS338/Data/FirstDownload/SweddDTMRIRAW/PPMI"
 
 #instantiate an object of RenameImages
 obj = RenameImages(rootdir)
 
-obj.check_names()
+# obj.check_names()
 obj.rename_images()
-obj.check_names()
+# obj.check_names()
 
